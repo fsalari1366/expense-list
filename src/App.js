c63a11b05dc637b83c9c0f94af7f44956d74ea3c
faride_salari <fsalarimanesh@gmail.com>
2023-08-23 22:00:15 +0330

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Expenses from './components/expenses/Expenses';
 
-function App() {
+export const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.45,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 594.12,
+      date: new Date(2021, 7, 10),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk',
+      amount: 450,
+      date: new Date(2021, 5, 11),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Get started!</h2>
+     <Expenses items={expenses} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
+export const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('clicked!');
+  };
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  )
+}
+
+export default ExpenseItem;
